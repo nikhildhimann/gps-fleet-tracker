128a2767699dae3537daf435c5a4b8b5302a44fa
nikhildhimann <nikhildhimaann@gmail.com>
2026-05-23 12:06:49 +0530

    Force dynamic rendering in root layout to bypass next build prerender errors

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/providers/ReduxProvider";
 
+export const dynamic = "force-dynamic";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
